f727b8a0cc354f1d636c03db43948d92f5a47d08
japhon <sysu.zhuang@gmail.com>
2015-07-16 23:55:13 +0800

    update v1.1

--- Source/Controller/RegisterManager.py
+++ Source/Controller/RegisterManager.py
@@ -5,4 +5,12 @@ from Config import settings
 from Config.url import urls
 from Model import UserTable
 
-
+def register(my_id, my_name, my_pass):
+    result = UserTable.select(my_id, 0, 'id')
+    
+    if result:
+        return 'Existed'
+    
+    if not result:
+        UserTable.insert(my_id, my_name, my_pass)
+            return 'RGSuccess'
